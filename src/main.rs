6971bf3cc1f7c4b679e56a667f90926ed27ce56c
Bruno Andrade <brunoan99@gmail.com>
2023-03-21 19:21:27 -0300

    add git status test

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::str;
 
 fn main() {
   let mut echo_hello = Command::new("sh");
-  echo_hello.arg("-c").arg("git s");
+  echo_hello.arg("-c").arg("git status --short");
   let hello_1 = echo_hello.output().expect("faield to execute process");
 
   println!("first_status: {:?}", hello_1.status);
