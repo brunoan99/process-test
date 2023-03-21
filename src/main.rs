3dae41d365993ea2b70066bd5a89f4df4a209e41
Bruno Andrade <brunoan99@gmail.com>
2023-03-21 18:24:09 -0300

    chore: start rust project

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use std::process::Command;
+use std::str;
+
+fn main() {
+  let mut echo_hello = Command::new("sh");
+  echo_hello.arg("-c").arg("git s");
+  let hello_1 = echo_hello.output().expect("faield to execute process");
+
+  println!("first_status: {:?}", hello_1.status);
+  println!();
+
+  println!(
+    "first_output: {:?}",
+    str::from_utf8(&hello_1.stdout).expect("failed to parse output")
+  );
+  println!();
+
+  println!(
+    "first_error: {:?}",
+    str::from_utf8(&hello_1.stderr).expect("failed to parse err")
+  );
+}
