d24643a7dc3a548a64aec8e4d44ccf435c8087e2
Bruno Andrade <brunoan99@gmail.com>
2023-03-23 20:51:56 -0300

    add echo resolving a path reference

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,41 @@
-use std::process::Command;
+use std::process::{Command, Output, Stdio};
 use std::str;
 
+fn print_output(title: String, o: &Output) {
+  println!("{}", title);
+  println!("status: {:?}", o.status);
+  println!(
+    "output: {:?}",
+    str::from_utf8(&o.stdout).expect("failed to parse output")
+  );
+  println!(
+    "error : {:?}",
+    str::from_utf8(&o.stderr).expect("failed to parse err")
+  );
+  println!();
+}
+
+fn git_status() {
+  let mut command = Command::new("sh");
+  command.args(["-c", "git status --short"]);
+  let output = command.output().expect("faield to execute command");
+
+  print_output(String::from("Git Status"), &output);
+}
+
+fn echo_path() {
+  let mut command = Command::new("sh");
+  command.args(["-c", "echo $HOME/workspace"]);
+  let mut output = command.output().expect("failed to execute command");
+  output
+    .stdout
+    .resize(output.stdout.len() - 1, output.stdout[0]);
+
+  print_output(String::from("Echo path with reference"), &output);
+}
+
 fn main() {
-  let mut echo_hello = Command::new("sh");
-  echo_hello.arg("-c").arg("git status --short");
-  let hello_1 = echo_hello.output().expect("faield to execute process");
+  git_status();
 
-  println!("first_status: {:?}", hello_1.status);
-  println!();
-
-  println!(
-    "first_output: {:?}",
-    str::from_utf8(&hello_1.stdout).expect("failed to parse output")
-  );
-  println!();
-
-  println!(
-    "first_error: {:?}",
-    str::from_utf8(&hello_1.stderr).expect("failed to parse err")
-  );
+  echo_path();
 }
